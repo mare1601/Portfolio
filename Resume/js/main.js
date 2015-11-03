aa2f141d8d9f41e2192090b756fbbcbee69e82f4
mare1601 <mary.e.ellis@gmail.com>
2015-11-03 16:24:08 -0500

    Update to include some print styles
    Already online

---
+++ Resume/js/main.js
@@ -0,0 +1,12 @@
+
+var $buoop = {
+	c:2,
+	text: "Hi! Your browser (%s) is a smidgen old.  You may want to <a%s>update</a> for the best experience.",
+}; 
+function $buo_f(){ 
+ var e = document.createElement("script"); 
+ e.src = "//browser-update.org/update.min.js"; 
+ document.body.appendChild(e);
+};
+try {document.addEventListener("DOMContentLoaded", $buo_f,false)}
+catch(e){window.attachEvent("onload", $buo_f)} 
