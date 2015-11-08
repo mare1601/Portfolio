6a23f1ff4d78e0fa8c11bb4365912d99ee8f96d0
mare1601 <mary.e.ellis@gmail.com>
2015-11-08 13:57:24 -0500

    Deleting some extra code
    No mouse over needed.

--- Resume/js/main.js
+++ Resume/js/main.js
@@ -1,25 +1,4 @@
 
-$(document).ready(function() {
-
-    $('.code-links a').bind('mouseover', function(){
-        $(this).parent('li').css({position:'relative'});
-        var img = $(this).children('img');
-        $('<div />').text(' ').css({
-            'height': img.height(),
-            'width': img.width(),
-            'background-color': 'orange',
-            'position': 'absolute',
-            'top': 22,
-            'left': 24,
-            'opacity': 0.5,
-            'border-radius': 50,
-        }).bind('mouseout', function(){
-            $(this).remove();
-        }).insertAfter(this);
-    });
-
-});
-
 var $buoop = {
 	c:2,
 	text: "Hi! Your browser (%s) is a smidgen old.  You may want to <a%s>update</a> for the best experience.",
