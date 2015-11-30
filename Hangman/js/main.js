e524abcd8fc5d65b58e0366b720fd6d42634d31b
mare1601 <mary.e.ellis@gmail.com>
2015-11-29 20:36:48 -0500

    Definite progress on hangman
    Need to pretty it up, and not allow the same letter guessed more than
    once…

--- Hangman/js/main.js
+++ Hangman/js/main.js
@@ -1,42 +1,83 @@
-var challenge = 'Catch me if you can';
+var challenge = prompt('Player 1 : Enter a phrase')
 var phrase = challenge.toUpperCase();
 var phraseSplit = phrase.split("");
 var phraseLength = phraseSplit.length;
 var phraseSlots = Array.apply(null, Array(phraseLength)).map(function () {});
+var usedLetters = [];
 var letterLocation = [];
+var spaceLocation = [];
+var failedAttempt = 0;
 
-console.log(phraseSplit);
-
+//Check first for any spaces in the phrase and store for later
+for(var i=0; i<phraseSplit.length; i++){
+  if(phraseSplit[i] === ' '){
+    spaceLocation.push(i);
+  }
+}
+for(var i=0; i < spaceLocation.length; i++){
+  for(var j=0; j< phraseSlots.length; j++){
+    phraseSlots[spaceLocation[i]] = " ";
+  }
+};
+//Populate the spaces in the html for every spot in the phrase
 for(var i=0; i < phraseSlots.length; i++){
-  if(phraseSlots[i]=== undefined){
-    var attempt = prompt('Enter a letter!');
+  var guessSpots = document.createElement('li')
+  guessSpots.setAttribute('class', 'slots');
+  if(phraseSlots[i] == undefined){
+    document.getElementById('letters').appendChild(guessSpots);
+  }
+  else if (phraseSlots[i] === " "){
+    var spaces = document.createTextNode(' ');
+    guessSpots.appendChild(spaces);
+    document.getElementById('letters').appendChild(guessSpots);
+  }
+};
+
+//Start the letter-checking cycle
+for(var i=0; i < phraseSlots.length; i++){
+    do {
+    var attempt = prompt('Player 2 : Enter a letter!');
     var letter = attempt.toUpperCase();
+    //Store the letters so they can't be re-guessed
+    usedLetters.push(letter);
     //Entering letters and checking to see if they exist in the phrase
     for(var i=0; i < phraseSplit.length; i++){
-      if(phraseSplit[i] === letter){
-        letterLocation.push(i+1);
+      if(phraseSplit[i] === letter && phraseSplit[i] !== usedLetters){
+        letterLocation.push(i);
       }
     };
     if (letterLocation[0] == null){
       alert('No ' + letter + ' in this phrase! Gettin dangerous!');
+      failedAttempt += 1;
+      if(failedAttempt >= 6){
+        alert('Oh no! RIP you!');
+        window.location.reload(true);
+        {break;};
+      };
     };
-    //Putting the letter where it goes
+    //Putting the letter where it goes in the phraseSlot array
     for (var i=0; i < letterLocation.length; i++){
       for (var j=0; j < phraseSlots.length; j++){
-          phraseSlots[letterLocation[i]] = letter;
+            phraseSlots[letterLocation[i]] = letter;
         }
     };
+letterLocation=[];
     //Printing the phrase
-    for(var i=1; i < phraseSlots.length+1; i++){
-      if(phraseSlots[i] === undefined){
-        document.write(' _ ');
-      }
-      else {
-        document.write(phraseSlots[i]);
-      }
+      for(var i=0; i < phraseSlots.length; i++){
+        if(phraseSlots[i] === " "){
+          var spaces = document.createTextNode(' ');
+          guessSpots.appendChild(spaces);
+          document.getElementById('letters').appendChild(guessSpots);
+        }
+        else if(phraseSlots[i] == letter){
+          var letterPlaced = document.createTextNode(letter);
+          document.getElementsByClassName('slots')[i].appendChild(letterPlaced);
+        }
+      };
     }
-  }
-  else {
-    alert('You saved yourself! Congrats!');
-  }
+    //See if the arrays match - when they do you've won the game!
+    while(JSON.stringify(phraseSlots)==JSON.stringify(phraseSplit) != true);
 }
+if (JSON.stringify(phraseSlots)==JSON.stringify(phraseSplit)==true){
+        alert('You saved yourself! Congrats!');
+      }
